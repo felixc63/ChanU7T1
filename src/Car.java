4f8a11fbf86b5863573c60730b6852635ab92d87
Jonathan Zhang <jonathanz28@nycstudents.net>
2023-01-31 12:30:49 -0500

    Done

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class Car {
+public class Car {
+    private String model;
+    private int miles;
+
+    public Car(String model, int miles){
+        this.model = model;
+        this.miles = miles;
+    }
+
+    public String toString(){
+        return model + " " + miles + "mi";
+    }
 }
--- src/UsedCarLot.java
+++ src/UsedCarLot.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class UsedCarLot {
+import java.util.ArrayList;
+
+public class UsedCarLot {
+    private ArrayList<Car> inventory;
+
+    public UsedCarLot(){
+        inventory = new ArrayList<Car>();
+    }
+
+    public ArrayList<Car> getInventory(){
+        return inventory;
+    }
+
+    public void addCar(Car car){
+        inventory.add(car);
+    }
+
+    public boolean swap(int index1, int index2){
+        if(index1 < inventory.size() && index2 < inventory.size()){
+            Car car1 = inventory.get(index1);
+            Car car2 = inventory.get(index2);
+            inventory.set(index1, car2);
+            inventory.set(index2, car1);
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
