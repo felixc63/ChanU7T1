7a3a06ace0be4c17e4d928c17d696e9a745c923a
Jonathan Zhang <jonathanz28@nycstudents.net>
2023-02-01 11:59:43 -0500

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,26 @@
 import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
-        ArrayList<String> names = new ArrayList<String>();
-        names.add("Ben");
-        names.add("Andy");
-        names.add("David");
-        String[] moreNames = {"Crystal", "Monica", "Toby", "Nancy"};
-        int namesAmt = names.size();
-        int moreNamesAmt = moreNames.length;
-        int thirdNameLength = names.get(2).length();
-        int secondNameLength = moreNames[1].length();
-        int lengths = namesAmt + moreNamesAmt + thirdNameLength + secondNameLength;
-        System.out.println(lengths); // should print 18
+        ArrayList<Integer> numList = new ArrayList<Integer>();
+
+        numList.add(3);
+
+        numList.add(0, 6);
+
+        numList.add(7);
+
+        numList.add(2, 8);
+
+        numList.set(3, 9);
+
+        numList.add(1, 2);
+
+        numList.add(1, numList.size());
+
+        numList.remove(4);
+
+        numList.set(numList.size()-1, 0);
+
+        System.out.println(numList);
     }
 }
--- src/UsedCarLot.java
+++ src/UsedCarLot.java
@@ -26,4 +26,68 @@ public class UsedCarLot {
             return false;
         }
     }
+
+    /* Adds a Car to the the inventory list at the index specified
+     by indexToAdd; this method increases the size of inventory by 1
+
+     PRECONDITION: indexToAdd < inventory.size()
+   */
+    public void addCar(int indexToAdd, Car carToAdd)
+    {
+        inventory.add(indexToAdd, carToAdd);
+    }
+
+    /* "sells" the Car located at indexOfCarToSell which
+        removes it from the inventory list and shifting the remaining
+        Cars in the inventory list to the left to fill in the gap;
+        this method reduces the size of inventory by 1
+
+        @return returns the Car that is being "sold" (removed from lot)
+
+        PRECONDITION: indexOfCarToSell < inventory.size()
+
+        HINT: the ArrayList remove() method returns the removed element...
+      */
+    public Car sellCarShift(int indexOfCarToSell)
+    {
+        return inventory.remove(indexOfCarToSell);
+    }
+
+    /* "sells" the Car located at indexOfCarToSell,
+       but instead of removing it and shifting the inventory
+       list to the left, REPLACE the Car at indexOfCarToSell
+       with NULL, thus creating an "empty parking spot" on the lot;
+       this method does NOT reduce the size of inventory by 1
+
+       PRECONDITION: indexOfCarToSell < inventory.size()
+
+       @return returns the Car that is being "sold" (replaced with null)
+     */
+    public Car sellCarNoShift(int indexOfCarToSell)
+    {
+        Car car = inventory.remove(indexOfCarToSell);
+        inventory.add(indexOfCarToSell, null);
+        return car;
+    }
+
+    /* moves Car located at index indexOfCarToMove to index destinationIndex;
+       if destinationIndex > indexOfCarToMove, moves the Car to the right in
+       inventory list; if destinationIndex < indexOfCarToMove, moves the
+       Car to the left in inventory list
+
+       PRECONDITIONS: indexOfCarToMove < inventory.size()
+                      destinationIndex < inventory.size()
+     */
+    public void moveCar(int indexOfCarToMove, int destinationIndex)
+    {
+        Car car = inventory.get(indexOfCarToMove);
+        if(destinationIndex > indexOfCarToMove){
+            inventory.add(destinationIndex+1, car);
+            inventory.remove(indexOfCarToMove);
+        }else{
+            inventory.add(destinationIndex, car);
+            inventory.remove(indexOfCarToMove+1);
+        }
+    }
+
 }
